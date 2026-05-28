aa73a7016508035e67336663e38c6640cdbe60bc
Marco Olinger <marco_olinger@hotmail.com>
2026-05-27 21:32:03 -0300

    Ajuste de dados

--- MiniProjeto_MarcoOlinger_AnaliseDeDados_Turma1.py
+++ MiniProjeto_MarcoOlinger_AnaliseDeDados_Turma1.py
@@ -22,8 +22,39 @@ if 'PR_CAT' in df_varejo.columns:
     df_varejo['PR_CAT'] = df_varejo['PR_CAT'].astype(str).str.strip().str.capitalize()
 print(df_varejo)
 
-
 #Conversão da coluna Data de String para Date/Time
 df_varejo['DATA_CONVERTIDA'] = pd.to_datetime(df_varejo['DATA'], format='%d/%m/%Y', errors='coerce')                                          
+
 print(df_varejo.info())
 print(df_varejo.head)
+
+# Verificação de linhas com campos  vazios
+# 1. Transformar espaço em branco em NaN real
+df_varejo_analise = df_varejo.replace(r'^\s*$', np.nan, regex=True)
+
+# 2. Exibir o total real de campos vazios por coluna
+print("Total de vazios por coluna:")
+print(df_varejo_analise.isna().sum())
+
+# Remoção de linhas duplicadas
+print(f"\nQuantidade de linhas duplicadas: {df_varejo.duplicated().sum()}")
+# Listagem de duplicadas
+# Mostra todas as linhas duplicadas (mantendo a primeira ocorrência oculta)
+linhas_duplicadas = df_varejo[df_varejo.duplicated()]
+print(linhas_duplicadas)
+print('')
+print('Análise estatística da coluna CL_FHL')
+if 'CL_FHL' in df_varejo.columns:
+    estatisticas_filhos = df_varejo['CL_FHL'].describe()
+    moda_filhos = df_varejo['CL_FHL'].mode()[0]
+    
+    print(f"Contagem: {estatisticas_filhos['count']}")
+    print(f"Média: {estatisticas_filhos['mean']:.2f}")
+    print(f"Mediana: {df_varejo['CL_FHL'].median()}")
+    print(f"Moda: {moda_filhos}")
+    print(f"Desvio Padrão: {estatisticas_filhos['std']:.2f}")
+    print(f"Mínimo: {estatisticas_filhos['min']}")
+    print(f"Quartil 25%: {estatisticas_filhos['25%']}")
+    print(f"Quartil 50% (Mediana): {estatisticas_filhos['50%']}")
+    print(f"Quartil 75%: {estatisticas_filhos['75%']}")
+    print(f"Máximo: {estatisticas_filhos['max']}")
