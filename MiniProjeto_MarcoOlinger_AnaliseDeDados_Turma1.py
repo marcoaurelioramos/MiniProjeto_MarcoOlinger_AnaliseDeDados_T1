38c53400452f81f9dba77c17e5795eb0ed651c56
Marco Olinger <marco_olinger@hotmail.com>
2026-05-28 20:02:47 -0300

    Continação da Limpeza do df

--- MiniProjeto_MarcoOlinger_AnaliseDeDados_Turma1.py
+++ MiniProjeto_MarcoOlinger_AnaliseDeDados_Turma1.py
@@ -1,56 +1,95 @@
+# Mini Projeto de Análise de Dados - Marco Olinger
+# Turma 1 - Análise de Dados
+
+#Importação das bibliotecas necessárias
 import pandas as pd
 import numpy as np
 
+# Carregamento da base de dados
 df_varejo = pd.read_csv('D:\\SCTEC\\Base Varejo.csv',sep=';')
 
 print(df_varejo.head())
 print(df_varejo.shape)
 print(df_varejo.info())
+print()
+print('################################################################################')
+
 
 # Verificação de Inconsistências Iniciais 
 print("Valores nulos por coluna:")
 print(df_varejo.isnull().sum())
 print(f"\nQuantidade de linhas duplicadas: {df_varejo.duplicated().sum()}")
+print()
+print('################################################################################')
+
 
 # Verifiquei que foram importadas colunas em branco, farei a remoção delas.
-df_varejo = df_varejo.drop(['Unnamed: 10', 'Unnamed: 11','Unnamed: 12','Unnamed: 13'], axis=1)
-print(df_varejo.info())
+df_varejo_limpo = df_varejo.drop(['Unnamed: 10', 'Unnamed: 11','Unnamed: 12','Unnamed: 13'], axis=1)
+print(df_varejo_limpo.info())
 
 
+print()
+print('################################################################################')
 # Remoção espaços em branco extras na coluna PR_CAT
-if 'PR_CAT' in df_varejo.columns:
-    df_varejo['PR_CAT'] = df_varejo['PR_CAT'].astype(str).str.strip().str.capitalize()
-print(df_varejo)
+if 'PR_CAT' in df_varejo_limpo.columns:
+    df_varejo_limpo['PR_CAT'] = df_varejo_limpo['PR_CAT'].astype(str).str.strip().str.capitalize()
+print(df_varejo_limpo)
 
+
+print()
+print('################################################################################')
 #Conversão da coluna Data de String para Date/Time
-df_varejo['DATA_CONVERTIDA'] = pd.to_datetime(df_varejo['DATA'], format='%d/%m/%Y', errors='coerce')                                          
+df_varejo_limpo['DATA_CONVERTIDA'] = pd.to_datetime(df_varejo_limpo['DATA'], format='%d/%m/%Y', errors='coerce')                                          
 
-print(df_varejo.info())
-print(df_varejo.head)
+print(df_varejo_limpo.info())
+print(df_varejo_limpo.head)
 
+
+print()
+print('################################################################################')
 # Verificação de linhas com campos  vazios
 # 1. Transformar espaço em branco em NaN real
-df_varejo_analise = df_varejo.replace(r'^\s*$', np.nan, regex=True)
+df_varejo_limpo_analise = df_varejo_limpo.replace(r'^\s*$', np.nan, regex=True)
 
 # 2. Exibir o total real de campos vazios por coluna
 print("Total de vazios por coluna:")
-print(df_varejo_analise.isna().sum())
+print(df_varejo_limpo_analise.isna().sum())
 
+
+print()
+print('################################################################################')
 # Remoção de linhas duplicadas
-print(f"\nQuantidade de linhas duplicadas: {df_varejo.duplicated().sum()}")
+print(f"\nQuantidade de linhas duplicadas: {df_varejo_limpo.duplicated().sum()}")
 # Listagem de duplicadas
-# Mostra todas as linhas duplicadas (mantendo a primeira ocorrência oculta)
-linhas_duplicadas = df_varejo[df_varejo.duplicated()]
+print()
+linhas_duplicadas = df_varejo_limpo[df_varejo_limpo.duplicated()]
 print(linhas_duplicadas)
-print('')
+df_varejo_limpo= df_varejo_limpo.drop_duplicates()
+print(f"\nQuantidade de linhas duplicadas após limpeza: {df_varejo_limpo.duplicated().sum()}")
+print()
+print(df_varejo_limpo.info())
+
+print()
+print('################################################################################')
+
+#Agrupamento dos dados por cliente para análise da quantidade de filhos (CL_ID) e a quantidade de filhos (CL_FHL)
+df_cli_agrupado = df_varejo_limpo.groupby('CL_ID', as_index=False)[['CL_FHL']].max()
+
+
+# Exibir o resultado
+print(df_cli_agrupado)
+
+print('################################################################################') 
+
+
 print('Análise estatística da coluna CL_FHL')
-if 'CL_FHL' in df_varejo.columns:
-    estatisticas_filhos = df_varejo['CL_FHL'].describe()
-    moda_filhos = df_varejo['CL_FHL'].mode()[0]
+if 'CL_FHL' in df_cli_agrupado.columns:
+    estatisticas_filhos = df_cli_agrupado['CL_FHL'].describe()
+    moda_filhos = df_cli_agrupado['CL_FHL'].mode()[0]
     
     print(f"Contagem: {estatisticas_filhos['count']}")
     print(f"Média: {estatisticas_filhos['mean']:.2f}")
-    print(f"Mediana: {df_varejo['CL_FHL'].median()}")
+    print(f"Mediana: {df_cli_agrupado['CL_FHL'].median()}")
     print(f"Moda: {moda_filhos}")
     print(f"Desvio Padrão: {estatisticas_filhos['std']:.2f}")
     print(f"Mínimo: {estatisticas_filhos['min']}")
